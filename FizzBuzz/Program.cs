71de080e40079ed8efc191e15a3e35079eb50d93
Braden Jonsson <braden337@gmail.com>
2021-03-03 20:30:49 -0600

    Finish lab 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 101; i++)
+            {
+                bool word = false;
+
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                    word = true;
+                }
+
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                    word = true;
+                }
+
+                if (word)
+                    Console.WriteLine();
+                else
+                    Console.WriteLine(i);
+            }
         }
     }
 }
